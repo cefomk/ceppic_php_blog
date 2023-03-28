188eb710ee3fdc9d0d75457af6ec15531c360d7b
cefomk <cefomk@kermed.net>
2023-03-28 11:46:24 +0200

    Import du schema sql de la base de donnée

--- data/astronomie.sql
+++ data/astronomie.sql
@@ -1,49 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: bsd:3306
--- Generation Time: Mar 17, 2023 at 05:17 PM
--- Server version: 10.11.2-MariaDB-1:10.11.2+maria~ubu2204
--- PHP Version: 8.1.15
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `astronomie`
---
 CREATE DATABASE IF NOT EXISTS `astronomie` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `astronomie`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `article`
---
 
 CREATE TABLE `article` (
   `id_article` int(11) NOT NULL,
   `titre` varchar(255) DEFAULT NULL,
   `contenu` text DEFAULT NULL,
   `image` varchar(500) DEFAULT NULL,
-  `id_utilisateur` int(11) NOT NULL,
+  `utilisateur_id` int(11) DEFAULT 15,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `modified_at` datetime NOT NULL DEFAULT current_timestamp()
+  `modified_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `utilisateur`
---
 
 CREATE TABLE `utilisateur` (
   `id_utilisateur` int(11) NOT NULL,
@@ -55,48 +30,24 @@ CREATE TABLE `utilisateur` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `article`
---
 ALTER TABLE `article`
   ADD PRIMARY KEY (`id_article`),
-  ADD KEY `articles_ibfk_1` (`id_utilisateur`);
+  ADD KEY `articles_ibfk_1` (`utilisateur_id`);
 
---
--- Indexes for table `utilisateur`
---
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`id_utilisateur`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `article`
---
 ALTER TABLE `article`
-  MODIFY `id_article` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_article` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `utilisateur`
---
 ALTER TABLE `utilisateur`
-  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `article`
---
 ALTER TABLE `article`
-  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`) ON DELETE CASCADE;
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id_utilisateur`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
